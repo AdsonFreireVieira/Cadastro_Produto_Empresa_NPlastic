4f6c05e553da1db420c49b5b2fff5cda21feac15
Adson <adsonfr.vieira@hotmail.com>
2025-02-26 21:27:54 -0300

    Correcao MapStruct

--- Nplastic/src/main/java/com/NPlastic/mapper/Usermapper.java
+++ Nplastic/src/main/java/com/NPlastic/mapper/Usermapper.java
@@ -6,6 +6,7 @@ import com.NPlastic.dto.UserResponseDTO;
 import org.mapstruct.Mapper;
 import org.mapstruct.Mapping;
 import org.mapstruct.factory.Mappers;
+import org.springframework.data.repository.query.parser.Part;
 
 
 import java.util.List;
@@ -15,8 +16,19 @@ public interface  Usermapper {
 
         Usermapper  INSTANCE= Mappers.getMapper(Usermapper.class);
 
+        @Mapping(source = "id",target = "id")
+        @Mapping(source = "nome",target = "nome")
+        @Mapping(source = "email",target = "email")
+        @Mapping(source = "telefone",target = "telefone")
+        @Mapping(source = "senha",target = "senha")
+        @Mapping(source = "data",target = "data")
     User toUSer(UserRequestDTO userRequestDTO);
 
+    @Mapping(source = "nome",target = "nome")
+    @Mapping(source = "email",target = "email")
+    @Mapping(source = "telefone",target = "telefone")
+    @Mapping(source = "senha",target = "senha")
+    @Mapping(source = "data",target = "data")
      UserResponseDTO toResponseDto(User user);
 
 }
